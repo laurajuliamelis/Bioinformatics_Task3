d59893311feaf641d301d2cfce5166ead81fcaa5
Laura Julia Melis <laaurajulia@gmail.com>
2020-11-10 19:10:16 +0100

    Removed tildes

--- seqBio/R/package.R
+++ seqBio/R/package.R
@@ -1,18 +1,17 @@
 #' seqBio: A sequence alginment package.
 #'
-#' El paquete seqBio contiene una función llamada \code{seq_alignment()}. 
+#' El paquete seqBio contiene una funcion llamada \code{seq_alignment()}. 
 #' 
-#' @section Class object:
-#' 
-#' La función \code{seq_alignment()} es útil para estudiar la significación estadística de un alineamiento por parejas.
-#' Para más información ir a \code{?seq_alignment} o bien \code{browseVignettes('seqBio')}.
+#' @section Object:
+#' La funcion \code{seq_alignment()} es util para estudiar la significacion estadistica de un alineamiento por parejas.
+#' Para mas informacion ir a \code{?seq_alignment} o bien \code{browseVignettes('seqBio')}.
 #' 
 #' @author
 #' Laura Julia Melis.
 #' 
-#' Curs de Fonaments de Bioinformàtica.
+#' Curs de Fonaments de Bioinformatica.
 #' 
-#' Master en Estadística i Investigació Operativa, Universitat de Barcelona.
+#' Master en Estadistica i Investigacio Operativa, Universitat de Barcelona.
 #' 
 #' @docType package
 #' @name seqBio
