cb383378ae5f37bfec0b1c3a3f830bbf7eab9d89
Laura Julia Melis <laaurajulia@gmail.com>
2020-11-10 19:47:14 +0100

    .

--- seqBio/vignettes/vignette.R
+++ seqBio/vignettes/vignette.R
@@ -1,8 +1,10 @@
+## ---- eval = FALSE------------------------------------------------------------
+#  #Comprobación de los inputs:
+#  stopifnot(class(seq1) == "character", class(seq2) == "character",
+#            seq_type %in% c("protein", "dna"), seq_align  %in% c("local", "global"),
+#            class(gap) == "numeric", class(N) == "numeric", class(shuff) == "numeric",
+#            length(gap) == 2, length(N) == 1, shuff %in% c(1, 2))
+
 ## ----include=FALSE------------------------------------------------------------
 #hello
-
-## -----------------------------------------------------------------------------
-# Loading the dataset.
-data(iris) 
-
 
