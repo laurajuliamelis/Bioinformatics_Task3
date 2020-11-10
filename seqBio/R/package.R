1ad191701b29859a5c3b443fd48e05951342d7a2
Laura Julia Melis <laaurajulia@gmail.com>
2020-11-10 18:32:07 +0100

    Corrected warning message

---
+++ seqBio/R/package.R
@@ -0,0 +1,18 @@
+#' seqBio: A sequence alginment package.
+#'
+#' El paquete seqBio contiene una función llamada \code{seq_alignment()}. Esta función es 
+#' útil para estudiar la significación estadística de un alineamiento por parejas.
+#' 
+#' @section Class object:
+#' \code{linreg()} calculates various statistics related to the linear regression model and contains multiple methods to obtain the regression coefficients or the predicted values, among others.
+#' 
+#' @author
+#' Laura Julia Melis.
+#' 
+#' Curs de Fonaments de Bioinformàtica.
+#' 
+#' Master en Estadística i Investigació Operativa, Universitat de Barcelona.
+#' 
+#' @docType package
+#' @name seqBio
+NULL
