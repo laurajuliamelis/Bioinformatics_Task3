6a25121f9376d209fbddb4eb3026188eba6e160a
Laura Julia Melis <laaurajulia@gmail.com>
2020-11-10 17:37:05 +0100

    Updating function seq_alignment()

--- seqBio/R/seq_alignment.R
+++ seqBio/R/seq_alignment.R
@@ -12,13 +12,16 @@
 #' @param N Número de replicas.
 #' @param shuff Secuencia donde se hace shuffling: 1 o 2.
 #'
-#' @return  \code{knapsack_dynamic} returns a list with two elements: the elements added to the knapsack and the maximum knapsack value.
+#' @return  \code{seq_alignment} devuelve un resultado gráfico (histograma de las puntiaciones estimadas) y un resultado numérico.
 #'
 #' @examples
-#' seq_alignment()
-#' seq_alignment()
-#' seq_alignment()
-#' seq_alignment()
+#' 
+#' seq_alignment(seq1 = "P0DP27.fa", seq2 = "Q9N1R0.fa", seq_type ="protein",
+#'               seq_align = "local", mat = data("BLOSUM62"), gap = c(-12, -2), N = 100, shuff = 1)
+#' seq_alignment(seq1 = "gi32121095_N_0.fa", seq2 = "gi32121095_N_1.fa", seq_type ="dna",
+#'               seq_align = "global", mat = data("BLOSUM6100"), gap = c(-8, -1), N = 2000, shuff)
+#' seq_alignment(seq1 = "P69905.fa", seq2 = "P01942.fa", seq_type ="protein",
+#'               seq_align = "global", mat = data("BLOSUM62"), gap = c(10, 0.5), N =1000, shuff=1)
 #' 
 #' @references \url{https://a-little-book-of-r-for-bioinformatics.readthedocs.io/en/latest/src/chapter4.html}
 #'
@@ -45,7 +48,7 @@ seq_alignment <- function (seq1, seq2, seq_type = c("protein", "dna"), seq_align
   generateSeqsWithMultinomialModel <- function(inputsequence, X){
     
     # Change the input sequence into a vector of letters
-    require("seqinr") # This function requires the SeqinR package.
+    require("seqinr", quietly = T) # This function requires the SeqinR package.
     inputsequencevector <- s2c(inputsequence)
     
     # Find the frequencies of the letters in the input sequence "inputsequencevector":
@@ -75,7 +78,7 @@ seq_alignment <- function (seq1, seq2, seq_type = c("protein", "dna"), seq_align
   }
   
   # 4. Alineamiento original (óptimo):
-  alignment <- pairwiseAlignment(s1, s2, type = seq_align, substitutionMatrix = mat, 
+  S <- pairwiseAlignment(s1, s2, type = seq_align, substitutionMatrix = mat, 
                              gapOpening = gap[1], gapExtension = gap[2], scoreOnly = TRUE)  
   
   # 5. Shuffling:
@@ -100,68 +103,78 @@ seq_alignment <- function (seq1, seq2, seq_type = c("protein", "dna"), seq_align
     }
     
   }
+  # 6. Cálculos:
   
-  # 6. Histograma
-  hist(randomscores, col="red")
+  ### 6.1. Media y desv estándar de las puntiaciones.
+  x <- mean(randomscores)
+  s <- sd(randomscores)
   
+  ### 6.2. Estimación de los parámetros de la distribución Gumbel.
+  lambda <- 1.2825 / s
+  u <- x - (0.45*s)
+  
+  ### 6.3. Estimación de K
+  m <- width(s1)
+  n <- width(s2)
+  K <- exp( (lambda*u) ) / (m*n)
+  
+  ### 6.4. Estandarización de la puntuación S
+  S_prima <- (lambda * S) - log( (K*m*n) )
+  
+  ### 6.5. Probabilidad que un alineamiento de secuencias al azar tenga una
+  ###      puntuación superior a la obtenida por azar
+  prob <- 1 - exp( exp(-S_prima)) 
+  
+  # 6. RESULTADO GRÁFICO
+  require("reliaR", quietly = T)
+  require("gplots", quietly = T) 
+  
+  dens <- dgumbel(randomscores, mu = u, sigma = lambda, log = FALSE)
+  gens <- lambda * exp((-lambda)*(randomscores-u) - exp((-lambda)*(randomscores-u)))
+  histo <- hist(randomscores, col="lightblue", breaks = 20, freq = FALSE, plot= FALSE)
+  y <- histo$density[max(which(histo$mids <= S))]
+  
+  texto <- c(gsub('.{3}$', '', seq1), gsub('.{3}$', '', seq2), seq_type, seq_align, mat, as.character(paste(gap[1], "y", gap[2])), N, shuff,
+             round(lambda,4), round(u,4), round(K,4), S, round(S_prima,4), round(prob,4))
+  texto <- as.data.frame(texto)
+  rownames(texto) <- c("Nombre secuencia 1:", "Nombre secuencia 2:", "Tipo de alineamiento:", 
+                       "Tipo de secuencia;", "Matriz de substitución",  "Puntuación Gap:",
+                       "Número de réplicas;", "Secuencia shuffling:", "Parámetro lambda:", 
+                       "Parámetro u:", "Constante K:", "Score original:", "Score estandarizado (S'):", 
+                       "P(S')")
+  colnames(texto) <- " "
+  
+  if(.Platform$OS.type == "unix") {
+    quartz(width = 8, height = 7)
+  } else {
+    windows(width = 10)
+  }
+  
+  par(mfrow=c(1,2), oma = c(0, 0, 2, 0))
+  hist(randomscores, col="lightblue", freq = FALSE, xaxt='n', main = NULL)
+  axis(1, at = seq(range(randomscores)[1], range(randomscores)[2], by=10), las=1)
+  lines(dens, lty= 1, lwd = 1, col= "red")
+  text(x = S, y = (y+0.0115), labels = paste0("S=", S), cex = 0.75, font=2)
+  text(x = S, y = (y + 0.005), labels = sprintf('\u2193'), cex = 1, font=2)
+  
+  textplot(texto, valign = "top", halign = "right", cex =0.80)
+  title(paste("Histograma de los N =", N,"scores"), outer = TRUE, cex.main = 1.5)
+  
+  # 7. REUSLTADO NUMÉRICO
+  cat("\n\n")
+  txt1 <- "Resumen numérico de las puntuaciones obtenidas al hacer shuffling:"
+  cat(txt1, fill = TRUE)
+  cat(rep("=", nchar(txt1)), sep = "", fill = TRUE)
+  print(summary(randomscores))
+  
+  cat("\n\n")
+  txt2 <- "Otros resultados numéricos:"
+  cat(txt2, fill = TRUE)
+  cat(rep("=", nchar(txt2)), sep = "", fill = TRUE)
+  nums <- c(lambda, u, K, S, S_prima, prob)
+  nums <- as.data.frame(nums)
+  rownames(nums) <- c("lambda", "u", "K", "S", "S'", "P(S' >= x)")
+  colnames(nums) <- NULL
+  print(nums)
+  cat("\n\n")
 }
-
-
-
-
-seq1 <- "gi32141095_N_0.fa"
-seq2 <- "gi32141095_N_1.fa"
-seq_type ="dna"
-seq_align = "global"
-mat = nucleotideSubstitutionMatrix(match = 2, mismatch = -1, baseOnly = TRUE)
-gap = c(-2, -8)
-N = 100
-shuff = 1
-
-seq_alignment(seq1, seq2, seq_type, seq_align, mat, gap, N, shuff)
-
-library(evd)
-scores <- table(randomscores)
-moda <- as.numeric(names( which(scores == max(scores))[1]))
-dens <- dgumbel((randomscores*(-1)), loc = moda, sd(randomscores)) 
-
-hist(randomscores, col="grey", breaks = (N/5), freq = FALSE)
-lines(dens, lwd = 3)
-
-
-#### Resultado gráfico: 
-  
-# Obtener un histograma de los N (nº de replicas) scores obtenidos al hacer el alineamiento 
-# por parejas entre la secuencia intacta versus las secuencia shuffling. 
-
-## 1. Añadir al histograma la distribución de Gumbel que se ajusta e indicar con una flecha donde 
-##    se sitúa el score original en el gráfico. 
-
-## 2. Por otro lado, añadir dentro del gráfico como texto:
-### 2.1. La información básica del alineamiento realizado (inputs): 
-####     Nombre de las secuencias, 
-####     tipo de alineamiento, 
-####     tipo de secuencia, 
-####     matriz de substitución, 
-####     puntuación Gap, 
-####     número de réplicas y 
-####     secuencia donde se hace shuffling;  
-
-### 2.2. Los resultados del alineamiento (outputs): 
-####     parámetros de la distribución de Gumbel, 
-####     score original, 
-####     Probabilidad(distr Gumbel estimada > score original) y 
-####     Probablidad empirica(Histograma > score original)
-
-#### Resultado no gráfico:
-
-## 1. De los N scores obtenidos haciendo shuffling mostrar el resultado de aplicar la función summary(). 
-## 2. Por otra parte mostrar:
-####   los parámetros de la distribución de Gumbel ajustada a los datos, 
-####   la K, 
-####   la S, 
-####   la S’ y 
-####   la P(S’)
-
-
-
